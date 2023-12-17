bf0e802ab88c5a1c183e22df2fd7c98e1c6ab486
Oleksii Bohoslavskyi <oleks.bohos@gmail.com>
2023-12-17 21:43:58 +0200

    delete modal js

--- js/modal-mob.js
+++ js/modal-mob.js
@@ -1,18 +1,1 @@
-let n = 0;
-function changeStr(str) {
-  return function (newStr) {
-    str = `${++n}+${str}${newStr}`;
-    return str;
-  };
-}
 
-const initStartStr = changeStr("Test");
-console.dir(initStartStr);
-
-setTimeout(() => {
-  const s1 = initStartStr("+ONE");
-  const s2 = initStartStr("+TWO");
-  const s3 = initStartStr("+THREE");
-  console.dir(initStartStr);
-}, 1000);
-// console.log("Result: ", change1, change2);
